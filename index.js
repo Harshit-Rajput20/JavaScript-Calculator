ce1f4ec51684f9417722aa62d7f2437494c7e7c4
Harshit-Rajput20 <harshitrajput7782gmail.com>
2023-08-11 23:24:48 +0530

    restored delete button functionality  prevented characters to be entered using keyboard

--- index.js
+++ index.js
@@ -2,21 +2,35 @@ let input = document.getElementById("inputBox");
 let buttons = document.querySelectorAll("button");
 let string = "";
 
+input.addEventListener("input", (e) => {
+  if (
+    (e.data >= "a" && e.data <= "z") ||
+    (e.data >= "A" && e.data <= "Z") ||
+    e.data === " "
+  ) {
+    console.log("Blocked: " + e.data);
+    input.value = input.value.slice(0, -1);
+    string = input.value;
+  }
+});
+
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
-      string = eval(string);
+      string = eval(input.value);
       input.value = string;
     } else {
-      string = string + e.target.innerHTML;
+      if (!(e.target.innerHTML === "DEL")) {
+        string = string + e.target.innerHTML;
+      }
       input.value = string;
     }
     if (e.target.innerHTML == "AC") {
       input.value = "";
       string = "";
     }
-    if (e.target.innerHTML == "DEL") {
-      string.slice(0, -2);
+    if (e.target.innerHTML === "DEL") {
+      string = string.slice(0, -1);
       input.value = string;
       console.log(string);
     }
